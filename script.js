8bfe476b42c5cbc5102b94608ea26a294eb506a4
Fanghan Hu <Fanghan.Hu@gmail.com>
2020-11-11 11:02:30 -0600

    outline time block and add my script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+/*
+Template for a single time block
+
+<div class="row rounded m-1">
+    <div class="col-2 bg-primary rounded-left text-white text-center d-flex flex-column justify-content-center">
+    12:00am
+</div>
+<div class="col bg-info d-flex flex-column justify-content-center">
+    <input class="bg-transparent border-0 text-white" type="text" placeholder="Add a task here....">
+    </div>
+    <div class="col-1 bg-warning text-center d-flex flex-column justify-content-center rounded-right text-white">
+    <h2><i class="fa fa-save"></i> </h2>
+</div>
+</div>
+*/
+
+/**
+ * @param hourStr leading hour string
+ * @param taskStr existing task
+ * @returns {jQuery.fn.init|jQuery|HTMLElement} the time block element
+ */
+function createTimeBlock(hourStr, taskStr) {
+
+}
